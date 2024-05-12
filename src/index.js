f441edf14c15858c08ceb1d13fb169395ca8af3d
DEVLevid <levidlima80@gmail.com>
2024-05-12 19:18:55 -0300

    feat: Create About component and add their styles.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header/Header'
 import Highlights from './components/Highlights/Highlights'
+import About from './components/About/About'
 import './global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,5 +10,6 @@ root.render(
   <React.StrictMode>
    <Header />
    <Highlights />
+   <About />
   </React.StrictMode>
 );
