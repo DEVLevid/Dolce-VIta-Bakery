aaeca4de01b6a20857ca981154ce95e3d87df522
DEVLevid <levidlima80@gmail.com>
2024-05-17 16:38:10 -0300

    feat: Adding media query for header component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,107 @@
-import React, { useState } from 'react';
-import styles from './styles.module.css';
-import logo from '../../Assets/logo.png';
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+import logo from "../../Assets/logo.png";
+import { IoMenuSharp } from "react-icons/io5";
+import classNames from "classnames";
+import { IoMdClose } from "react-icons/io";
 
 function Header() {
-    const [activeNav, setActiveNav] = useState("#home");
+  const [activeNav, setActiveNav] = useState("#home");
+  const [openMenu, setOpenMenu] = useState(false);
 
-    return (
-        <div className={styles.container} id='header'>  
-            <div className={styles.logo}>
-                <a href="">
-                    <img src={logo} alt="logo" className={styles.logoImg}/>
-                </a>
-            </div>
-            <div className={styles.options}>
-                <ul className={styles.option}>
-                    <li>
-                        <a href="#about" onClick={() => { setActiveNav("#about")}}>
-                            <h3>Sobre nós</h3>
-                        </a>
-                    </li>
-                </ul>
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
-                <ul className={styles.option}>
-                    <li>
-                        <a href="#menu" onClick={() => { setActiveNav("#menu")}}>
-                            <h3>Cardápio</h3>
-                        </a>
-                    </li>
-                </ul>
+  return (
+    <div className={styles.container} id="header">
+      <div className={styles.logo}>
+        <a href="#home">
+          <img src={logo} alt="logo" className={styles.logoImg} />
+        </a>
+      </div>
+      <div className={styles.options}>
+        <ul className={styles.option}>
+          <li>
+            <a href="#about" onClick={() => setActiveNav("#about")}>
+              <h3>Sobre nós</h3>
+            </a>
+          </li>
+        </ul>
 
-                <ul className={styles.option}>
-                    <li>
-                        <a href="#contact" onClick={() => { setActiveNav("#contact")}}>
-                            <h3>Contato</h3>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+        <ul className={styles.option}>
+          <li>
+            <a href="#menu" onClick={() => setActiveNav("#menu")}>
+              <h3>Cardápio</h3>
+            </a>
+          </li>
+        </ul>
+
+        <ul className={styles.option}>
+          <li>
+            <a href="#contact" onClick={() => setActiveNav("#contact")}>
+              <h3>Contato</h3>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.menuContainer}>
+        <IoMenuSharp className={styles.menuIcon} onClick={toggleMenu} />
+      </div>
+      <div
+        className={classNames(styles.menuContent, {
+          [styles.showMenuContent]: openMenu,
+        })}
+      >
+        <div className={styles.menuOptions}>
+          <div className={styles.close}>
+            <IoMdClose className={styles.closeIcon} onClick={toggleMenu} />
+          </div>
+          <ul className={styles.option}>
+            <li>
+              <a
+                href="#about"
+                onClick={() => {
+                  setActiveNav("#about");
+                  setOpenMenu(false);
+                }}
+              >
+                <h3>Sobre nós</h3>
+              </a>
+            </li>
+          </ul>
+
+          <ul className={styles.option}>
+            <li>
+              <a
+                href="#menu"
+                onClick={() => {
+                  setActiveNav("#menu");
+                  setOpenMenu(false);
+                }}
+              >
+                <h3>Cardápio</h3>
+              </a>
+            </li>
+          </ul>
+
+          <ul className={styles.option}>
+            <li>
+              <a
+                href="#contact"
+                onClick={() => {
+                  setActiveNav("#contact");
+                  setOpenMenu(false);
+                }}
+              >
+                <h3>Contato</h3>
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Header;
